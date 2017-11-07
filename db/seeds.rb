bd5992a713224df7802174bc7c1148114d256774
Jake Engel <jakeengel01@gmail.com>
2017-11-07 17:53:32 +0100

    Finished the rails wikinimous challenge.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+15.times do
+  article = Article.new(
+    title: Faker::RickAndMorty.character,
+    content: Faker::MostInterestingManInTheWorld.quote
+  )
+  article.save!
+end
